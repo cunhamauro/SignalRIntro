dba0005eb8ab5593fe35b7ddf841b9f706697b24
Mauro Cunha <cunhamauro@outlook.pt>
2025-01-01 15:09:10 +0000

    Add project files.

---
+++ SignalRIntro.Api/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.SignalR;
+using SignalRIntro.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR(); // Register SignalR specific services with dependency injection
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapPost("broadcast", async (string message, IHubContext<ChatHub, IChatClient> context) =>
+{
+    await context.Clients.All.ReceiveMessage(message);
+    return Results.NoContent();
+});
+
+app.UseHttpsRedirection();
+
+app.MapHub<ChatHub>("chat-hub");
+
+app.Run();
